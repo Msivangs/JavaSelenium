0a6453633147a2d10a5c222d819908a6cac4d5f4
Msivangs <62917298+Msivangs@users.noreply.github.com>
2021-10-01 10:58:25 -0500

    interfaces

---
+++ Java/Interface/AustralianTraffic.java
@@ -0,0 +1,37 @@
+
+public class AustralianTraffic implements InterfaceDemo{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InterfaceDemo a=new AustralianTraffic();
+		a.redStop();
+		a.flashYellow();
+		a.greenGo();
+	}
+
+	
+
+	@Override
+	public void redStop() {
+		// TODO Auto-generated method stub
+		// Write your code here
+		
+		System.out.println("Redstop method implemented");
+	}
+
+	@Override
+	public void flashYellow() {
+		// TODO Auto-generated method stub
+		// Write your code here
+		System.out.println("flashYellow method implemented");
+	}
+
+
+
+	@Override
+	public void greenGo() {
+		// TODO Auto-generated method stub
+		System.out.println("greenGo method implemented");
+	}
+
+}
---
+++ Java/Interface/InterfaceDemo.java
@@ -0,0 +1,9 @@
+
+//classes should define and implement methods present in interface
+
+public interface InterfaceDemo {
+	
+	public void greenGo();
+    public void redStop();
+    public void flashYellow();
+}
