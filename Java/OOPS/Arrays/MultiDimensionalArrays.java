530878cda62cd6d43a2a92a1f45c9c06df71331f
Msivangs <62917298+Msivangs@users.noreply.github.com>
2021-10-05 14:57:37 -0500

    multidimensionalarrays

---
+++ Java/OOPS/Arrays/MultiDimensionalArrays.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+public class MultiDimensionalArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// here a[row][column]
+
+		int a[][] = new int[2][3];
+
+		a[0][0] = 2;
+
+		a[0][1] = 4;
+
+		a[0][2] = 5;
+
+		a[1][0] = 3;
+
+		a[1][1] = 4;
+
+		a[1][2] = 7;
+
+		// System.out.println(a[1][0]);
+
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.println(a[i][j]);
+			}
+		}
+
+	}
+
+}
