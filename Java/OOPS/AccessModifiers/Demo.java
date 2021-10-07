6889cd30c367701743530e54be0be1d2da3bf3aa
Msivangs <62917298+Msivangs@users.noreply.github.com>
2021-10-07 11:44:35 -0500

    Accessmodifiersinjava

---
+++ Java/OOPS/AccessModifiers/Demo.java
@@ -0,0 +1,17 @@
+package AccessModifiers;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		//public, private, protected, default
+		
+		//default : We can access method anywhere in the package.
+		//public : variable/method as public then you will have access across all the packages
+		//private : You cannot access method or variable out side the class of same package
+		//protected : If you define variable or method as protected, you can access those in sub classes only (this applies for other packages)
+		
+	}
+
+}
