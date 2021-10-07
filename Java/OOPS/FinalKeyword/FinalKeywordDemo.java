437dd339b1551de50a446017561a15fe453f6bd6
Msivangs <62917298+Msivangs@users.noreply.github.com>
2021-10-06 21:57:11 -0500

    finalkeywordusageinjava

---
+++ Java/OOPS/FinalKeyword/FinalKeywordDemo.java
@@ -0,0 +1,16 @@
+package FinalKeyword;
+
+final class FinalKeywordDemo {  //If you mark class as a final you can inheritate this class
+	
+	final void getData() { //Here if you can make a method as final.
+		
+	}
+     
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//If you write any variable by using final keyword, its not possible to change it
+       final int i=4; //constant variable
+	}
+
+}
