d427f1046cf560bb50d14ce53ae38d6edbe67093
Msivangs <62917298+Msivangs@users.noreply.github.com>
2021-10-06 22:36:56 -0500

    packagesandtypesdemo

---
+++ Java/OOPS/Packages/demo.java
@@ -0,0 +1,12 @@
+package Packages;
+
+
+public class demo {
+	public void getData() {
+		System.out.println("This is packages demo");
+	}
+	//Java.lang is the default package comes in java when create the java package.
+	//java.util - collections and interfaces will come from this package.
+	
+
+}
---
+++ Java/OOPS/Packages2/Demo1.java
@@ -0,0 +1,14 @@
+package Packages2;
+
+import Packages.demo;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+demo abc=new demo();
+abc.getData();
+
+	}
+
+}
