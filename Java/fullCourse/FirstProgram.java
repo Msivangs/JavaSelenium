8356d38e87158b0ee42d36cf06561bb43b3501ea
Msivangs <62917298+Msivangs@users.noreply.github.com>
2021-10-14 11:43:00 -0500

    firstprogram

--- Java/fullCourse/FirstProgram.java
+++ Java/fullCourse/FirstProgram.java
@@ -1,10 +1,24 @@
 
 public class FirstProgram {
+	
+	
+	public void age() {
+		int a = 10;
+		String s="method";
+		boolean b = true;
+		System.out.println("Hello");
+		System.out.println(a);
+		System.out.println(s);
+		System.out.println(b);
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Hello world");
+    
+		FirstProgram fp=new FirstProgram();
+		fp.age();
+		
 	}
 
 }
