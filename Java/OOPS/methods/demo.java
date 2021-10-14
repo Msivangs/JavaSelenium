a3d904c4cf420aee97f39e94826d91a031486ebc
Msivangs <62917298+Msivangs@users.noreply.github.com>
2021-10-13 21:39:24 -0500

    Fullcoursefolderadded

---
+++ Java/OOPS/methods/demo.java
@@ -0,0 +1,18 @@
+package methods;
+
+public class demo {
+
+	int i; // declaration, initialize
+
+	public void display() { // declare a method
+
+		// all definitions goes here !!
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
